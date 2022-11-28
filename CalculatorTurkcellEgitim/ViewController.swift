def8213b53d009d0961e19a976542cd2eb6e878a
Alihan AÇIKGÖZ <alihanacikgoz1@hotmail.com>
2022-11-28 13:35:56 +0300

    Started to add action features on buttons.

--- CalculatorTurkcellEgitim/ViewController.swift
+++ CalculatorTurkcellEgitim/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var monitorLabel: UILabel!
+    @IBOutlet weak var cButton: UIButton!
+    
+    override func loadView() {
+        // Override edilen bir fonksyion olduğu için içerisine yazılanlardan önce mutlaka super.loadView()'i çağırmamız gerekmektedir.
+        super.loadView()
+        monitorLabel.text = "0"
+    }
+    
+    var firstNumber = Float()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func cButtonAction(_ sender: Any) {
+        self.monitorLabel.text = "0"
+    }
+    
+    @IBAction func plusMinusButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func modButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func divideButton(_ sender: Any) {
+        
+    }
 }
 
